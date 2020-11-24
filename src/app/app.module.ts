02981c4f59ea5a3766f2be1c97e0152d35099da3
Admin <kuduyablik@gmail.com>
2020-11-24 19:04:00 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {charaComponent} from  './chara/chara.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {signUpFormComponent } from './signUpForm/signUpForm.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    charaComponent,
+    NavbarComponent,
+    signUpFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
